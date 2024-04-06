dfdc03f4d0fc2adaad316851ec763b56ea347f24
ramarmdhn <rama.ramadhn24@gmail.com>
2024-04-06 23:47:07 +0700

    revisi validasi

--- application/views/form-matakuliah.php
+++ application/views/form-matakuliah.php
@@ -12,39 +12,37 @@
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card">
-                    <div class="card-header">
+                    <div class="card-header bg-warning">
                         Form Mata Kuliah
                     </div>
                     <div class="card-body">
                        <!-- <h2>Form Matakuliah</h2> -->
-                        <?php if(validation_errors()): ?>
-                            <div class="alert alert-danger" role="alert">
-                                <ul>
-                                    <?= validation_errors('<li>', '</li>') ?>
-                                </ul>
-                            </div>
-                        <?php endif; ?>
-                        <form action="<?= base_url('matakuliah/cetak')  ?>" method="POST">
+                     
+                        <form action="<?= base_url('matakuliah/cetak') ?>" method="POST">
                             <div class="form-group">
                                 <label for="kode">Kode MTK</label>
                                 <input type="text" name="kode" id="kode" class="form-control" placeholder="Masukkan Kode MTK" value="<?= set_value('kode') ?>">
+                                <?= form_error('kode', '<div class="text-danger">', '</div>') ?>
                             </div>
                             <div class="form-group">
                                 <label for="nama">Nama MTK</label>
                                 <input type="text" name="nama" id="nama" class="form-control" placeholder="Masukkan Nama MTK" value="<?= set_value('nama') ?>">
+                                <?= form_error('nama', '<div class="text-danger">', '</div>') ?>
                             </div>
                             <div class="form-group">
                                 <label for="sks">SKS</label>
                                 <select name="sks" id="sks" class="form-control">
                                     <option value="">-- PILIH SKS --</option>
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="4">4</option>
+                                    <option value="1" <?= set_select('sks', '1') ?>>1</option>
+                                    <option value="2" <?= set_select('sks', '2') ?>>2</option>
+                                    <option value="3" <?= set_select('sks', '3') ?>>3</option>
+                                    <option value="4" <?= set_select('sks', '4') ?>>4</option>
                                 </select>
+                                <?= form_error('sks', '<div class="text-danger">', '</div>') ?>
                             </div>
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </form>
+
                     </div>
                 </div>
             </div>
